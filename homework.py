8c721af2cd633c5d1a3c0e7e47bda5c6c6941e4d
Денис <zolkovd@yandex.ru>
2022-03-12 18:33:22 +0400

    first

--- homework.py
+++ homework.py
@@ -1,11 +1,23 @@
-...
+try:
+    import logging
+    import logging.config
+    import os
+    import sys
+    import time
+
+    import requests
+    import telegram
+    from dotenv import load_dotenv
+
+    import my_ex
+except ImportError as error:
+    logging.error(f'Ошибка при импорте {error}', exc_info=True)
 
 load_dotenv()
 
-
-PRACTICUM_TOKEN = ...
-TELEGRAM_TOKEN = ...
-TELEGRAM_CHAT_ID = ...
+PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
 RETRY_TIME = 600
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
@@ -18,66 +30,162 @@ HOMEWORK_STATUSES = {
     'rejected': 'Работа проверена: у ревьюера есть замечания.'
 }
 
+logger = logging.getLogger(__name__)
+
 
 def send_message(bot, message):
-    ...
+    logger.info('Сообщение отправляется')
+    try:
+        bot.send_message(
+            chat_id=TELEGRAM_CHAT_ID,
+            text=message,
+        )
+    except Exception as error:
+        raise my_ex.MessageSendError from error
+    else:
+        logger.info('Сообщение отправлено!')
 
 
 def get_api_answer(current_timestamp):
     timestamp = current_timestamp or int(time.time())
     params = {'from_date': timestamp}
-
-    ...
+    try:
+        response = requests.get(ENDPOINT, headers=HEADERS, params=params)
+    except Exception as error:
+        logger.exception('Эндпоинт шалит')
+        raise my_ex.ErrorWithEndpoint from error
+    else:
+        if response.status_code != 200:
+            raise my_ex.NotOKError(response.status_code)
+        return response.json()
 
 
 def check_response(response):
-
-    ...
+    if type(response['homeworks']) is not list:
+        raise my_ex.HomeworkNotList
+    if 'homeworks' not in response:
+        raise my_ex.HomeworksNotInList
+    else:
+        return response.get('homeworks')
 
 
 def parse_status(homework):
-    homework_name = ...
-    homework_status = ...
-
-    ...
-
-    verdict = ...
-
-    ...
-
+    homework_name = homework.get('homework_name')
+    homework_status = homework.get('status')
+    verdict = HOMEWORK_STATUSES[homework_status]
     return f'Изменился статус проверки работы "{homework_name}". {verdict}'
 
 
 def check_tokens():
-    ...
+    tokens = [
+        PRACTICUM_TOKEN,
+        TELEGRAM_TOKEN,
+        TELEGRAM_CHAT_ID
+    ]
+    return all(tokens)
+
+
+def send_error(error, bot):
+    message = f'Сбой в работе программы: {error}'
+    send_message(bot, message)
+    time.sleep(RETRY_TIME)
 
 
 def main():
     """Основная логика работы бота."""
 
-    ...
+    if check_tokens():
+        bot = telegram.Bot(token=TELEGRAM_TOKEN)
+        current_timestamp = 10
+    else:
+        logger.critical('Отсутствует обязательная переменная окружения')
+        sys.exit()
 
-    bot = telegram.Bot(token=TELEGRAM_TOKEN)
-    current_timestamp = int(time.time())
+    current_report = {'homework_name': 'current_status'}
 
-    ...
+    prev_report = {'homework_name': 'status'}
 
     while True:
         try:
-            response = ...
-
-            ...
-
-            current_timestamp = ...
+            response = get_api_answer(current_timestamp)
+            homeworks = check_response(response)
+            if len(homeworks) == 0:
+                logger.info('Вашу работа еще не взяли на проверку(')
+            else:
+                homework = homeworks[0]
+                current_status = parse_status(homework)
+                current_report.update(dict(
+                    homework_name=homework.get('homework_name'),
+                    current_status=current_status
+                ))
+                if current_report != prev_report:
+                    send_message(bot, current_status)
+                    prev_report = current_report.copy()
+                else:
+                    logger.debug('В ответе отсутсвуют новые статусы')
+            current_timestamp = int(time.time())
+        except my_ex.ErrorWithEndpoint as error:
+            logger.exception('Проблема с доступом к эндпоинту')
+            send_error(error, bot)
+            raise
+        except my_ex.HomeworksNotInList as error:
+            logger.exception('"homeworks" отсутствует')
+            send_error(error, bot)
+            raise
+        except my_ex.HomeworkNotList as error:
+            logger.exception('Под ключом "homework" – не лист')
+            send_error(error, bot)
+            raise
+        except my_ex.NotOKError as error:
+            logger.exception('Код ответа не 200')
+            send_error(error, bot)
+            raise
+        except my_ex.MessageSendError as error:
+            logger.exception('Ошибка при отправке сообщения')
+            send_error(error, bot)
+            raise
+        finally:
             time.sleep(RETRY_TIME)
-
-        except Exception as error:
-            message = f'Сбой в работе программы: {error}'
-            ...
-            time.sleep(RETRY_TIME)
-        else:
-            ...
 
 
 if __name__ == '__main__':
+    ERROR_LOG_FILENAME = '.program.log'
+
+    LOGGING_CONFIG = {
+        'version': 1,
+        'disable_existing_loggers': False,
+        'formatters': {
+            'default': {
+                'format': ('%(asctime)s : %(levelname)s : '
+                           '%(message)s : %(lineno)s')
+            },
+        },
+        'handlers': {
+            'logfile': {
+                'formatter': 'default',
+                'level': 'ERROR',
+                'class': 'logging.handlers.RotatingFileHandler',
+                'filename': ERROR_LOG_FILENAME,
+                'backupCount': 5,
+                'maxBytes': 50000000,
+            },
+            'verbose_output': {
+                'formatter': 'default',
+                'level': 'DEBUG',
+                'class': 'logging.StreamHandler',
+                'stream': 'ext://sys.stdout',
+            },
+        },
+        'loggers': {
+            'root': {
+                'handlers': ['logfile'],
+                'level': 'DEBUG',
+            },
+            '__main__': {
+                'handlers': ['verbose_output'],
+                'level': 'INFO',
+            },
+        },
+    }
+    logging.config.dictConfig(LOGGING_CONFIG)
     main()
---
+++ my_ex.py
@@ -0,0 +1,63 @@
+class NotOKError(Exception):
+    def __init__(self, code_status):
+        self.code_status = code_status
+        super().__init__(
+            f'API вернул код: {code_status}'
+        )
+
+
+class MissingRequiredTokens(Exception):
+    def __init__(self, tokens):
+        self.token = tokens
+        super().__init__(
+            f'Отсутствуют обязательные токены: {tokens}'
+        )
+
+
+class MessageSendError(Exception):
+    def __init__(self):
+        super().__init__(
+            'Ошибка при отправке сообщения'
+        )
+
+
+class MissingRequiredKeys(Exception):
+    def __init__(self):
+        super().__init__(
+            'Отсутствуют ожидаемые ключи в ответе API'
+        )
+
+
+class NoNewStatuses(Exception):
+    def __init__(self):
+        super().__init__(
+            'Нет новых статусов'
+        )
+
+
+class UndocumentedStatus(Exception):
+    def __init__(self):
+        super().__init__(
+            'API вернул недокументированный статус'
+        )
+
+
+class ErrorWithEndpoint(Exception):
+    def __init__(self):
+        super().__init__(
+            'Проблема с доступом к эндпоинту'
+        )
+
+
+class HomeworkNotList(Exception):
+    def __init__(self):
+        super().__init__(
+            'Под ключом "homeworks" не список'
+        )
+
+
+class HomeworksNotInList(Exception):
+    def __init__(self):
+        super().__init__(
+            '"Homeworks" отсутствует в словаре'
+        )
